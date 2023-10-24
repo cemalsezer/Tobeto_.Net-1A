e2bd905932e0392a98f2f5c49bd70515ac83c18d
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-10-24 20:54:33 +0300

    arrays is done

---
+++ CSharpCourse/Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// string[] students = new string[3];
+//students[0] = "Engin";
+//students[1] = "Derin";
+//students[2] = "Salih";
+// string[] students = new [] { "Engin", "Derin", "Salih" }; 
+
+string[] students = { "Engin", "Derin", "Salih" };
+
+foreach (var student in students)
+{
+    Console.WriteLine(student);
+}
+
+Console.WriteLine();
+Console.ReadLine();
+
+
+
+string[,] regions = new string[5, 3]
+{
+            {"İstanbul", "Balıkesir", "İzmit"},
+            {"Ankara", "Konya", "Kırıkkale"},
+            {"Antalya", "Adana", "Mersin"},
+            {"Rize", "Trabzon ", "Samsun"},
+            {"İzmir", "Manisa", "Muğla"}
+};
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i, j]);
+    }
+    Console.WriteLine("*****************");
+}
