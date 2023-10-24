eae820d063bb81e654cb65a486191f975daeb440
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-10-24 21:42:11 +0300

    classes is done

---
+++ CSharpCourse/Classes/Program.cs
@@ -0,0 +1,61 @@
+﻿CustomerManager customerManager = new CustomerManager(); 
+
+
+customerManager.Add();
+customerManager.Update();
+
+
+ProductManager productManager = new ProductManager(); 
+productManager.Add();
+productManager.Update();
+
+Customer customer = new Customer();
+customer.City = "Ankara";
+customer.Id = 1;
+customer.FirstName = "Cemal";
+customer.LastName = "Sezer";
+
+
+Customer customer2 = new Customer
+{
+    Id = 2,
+    City = "İzmir",
+    FirstName = "Derin",
+    LastName = "Demiroğ"
+};
+
+
+Console.WriteLine(customer2.FirstName);
+
+
+Console.ReadLine();
+
+
+
+
+class CustomerManager
+{
+
+    public void Add()
+    {
+        Console.WriteLine("Customer Added!");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Customer Updated!");
+    }
+}
+
+class ProductManager
+
+{
+    public void Add()
+    {
+        Console.WriteLine("Product Added!");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Product Updated!");
+    }
