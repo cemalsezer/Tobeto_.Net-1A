70ab904115b6f7123969f12b4398ff205548e9bd
Cemal Sezer <infocemalsezer@gmail.com>
2023-10-27 02:52:25 +0300

    interfaces is build

---
+++ CSharpCourse/Interfaces/Program.cs
@@ -0,0 +1,17 @@
+﻿interface IPerson
+{
+    string Name { get; set; }
+    int Id { get; set; }
+}
+
+class Customer : IPerson
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+}
+
+class Student:IPerson
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+}
--- CSharpCourse/RecapDemo/Form1.cs
+++ CSharpCourse/RecapDemo/Form1.cs
@@ -9,35 +9,39 @@ namespace RecapDemo
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int top=0;
+            GeneratedButtons();
+
+        }
+
+        private void GeneratedButtons()
+        {
+            int top = 0;
             int left = 0;
             Button[,] buttons = new Button[8, 8];
 
             for (int i = 0; i <= buttons.GetUpperBound(0); i++)
             {
-                for (int j = 0; j <=  buttons.GetUpperBound(1); j++)
+                for (int j = 0; j <= buttons.GetUpperBound(1); j++)
                 {
-                    buttons[i,j]= new Button();
+                    buttons[i, j] = new Button();
                     buttons[i, j].Width = 50;
                     buttons[i, j].Height = 50;
                     buttons[i, j].Left = left;
                     buttons[i, j].Top = top;
                     left += 50;
-                    if((i+j) % 2 == 0)
+                    if ((i + j) % 2 == 0)
                     {
                         buttons[i, j].BackColor = Color.Black;
                     }
                     else
                     {
-                        buttons[i,j].BackColor = Color.White;
+                        buttons[i, j].BackColor = Color.White;
                     }
-                    this.Controls.Add(buttons[i,j]);
+                    this.Controls.Add(buttons[i, j]);
                 }
                 top += 50;
                 left = 0;
             }
-
-          
         }
     }
 }
