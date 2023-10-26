d7d987969ca3fde74453efe2a154dd1322de151d
Cemal Sezer <infocemalsezer@gmail.com>
2023-10-27 02:13:27 +0300

    classes is done

---
+++ CSharpCourse/Classes/Customers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Classes;
+
+public class Customer
+{
+
+    public int Id { get; set; }
+    /*
+    private string _firstName;
+    public string FirstName
+    {
+        get { return "Mrs. " + _firstName; }
+        set { _firstName = value; }
+
+    }*/
+    public string FirstName { get; set; } // Auto-property
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
---
+++ CSharpCourse/Classes/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classes;
+
+class ProductManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Product Added");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Product Updated!");
+    }
+}
