020f518c84c257943b7505a4a1504041df387f1e
Cemal Sezer <infocemalsezer@gmail.com>
2023-10-27 03:17:03 +0300

    Interfaces is build

---
+++ CSharpCourse/Interfaces/ICustomerDal.cs
@@ -0,0 +1,80 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal interface ICustomerDal
+    {
+        void Add();
+        void Update();
+
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql updated");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql deleted");
+        }
+    }
+
+class OracleCustomerDal : ICustomerDal
+{
+    public void Add()
+    {
+        Console.WriteLine("Oracle added");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Oracle updated");
+    }
+
+    public void Delete()
+    {
+        Console.WriteLine("Oracle deleted");
+    }
+
+}
+
+class MySqlCustomerDal : ICustomerDal
+{
+    public void Add()
+    {
+        Console.WriteLine("MySql added");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("MySql updated");
+    }
+
+    public void Delete()
+    {
+        Console.WriteLine("MySql deleted");
+    }
+
+}
+
+class CustomerManager
+{
+    public void Add(ICustomerDal customerDal)
+    {
+        customerDal.Add();
+    }
+}
--- CSharpCourse/Interfaces/Program.cs
+++ CSharpCourse/Interfaces/Program.cs
@@ -1,17 +1,86 @@
-﻿interface IPerson
+﻿using Interfaces;
+
+ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+            };
+
+foreach (var customerDal in customerDals)
 {
-    string Name { get; set; }
+    customerDal.Add();
+}
+
+//InterfacesIntro();
+//Demo();
+static void InterfacesIntro()
+{
+    PersonManager manager = new PersonManager();
+    //manager.Add(new Customer{Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara"});
+    Customer customer = new Customer
+    {
+        Id = 1,
+        FirstName = "Engin",
+        LastName = "Demiroğ",
+        Address = "Ankara"
+    };
+
+    Student student = new Student
+    {
+        Id = 1,
+        FirstName = "Derin",
+        LastName = "Demiroğ",
+        Departmant = "Computer Sciences"
+    };
+
+    manager.Add(customer);
+    manager.Add(student);
+    }
+
+interface IPerson
+{
     int Id { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+
 }
 
 class Customer : IPerson
 {
-    public string Name { get; set; }
     public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Address { get; set; }
+
 }
 
 class Student:IPerson
 {
-    public string Name { get; set; }
     public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Departmant { get; set; }
+
+}
+
+
+class Worker : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Departmant { get; set; }
+
+}
+
+class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
 }
