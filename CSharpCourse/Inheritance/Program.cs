ec2986e8c00b627b32eedb8aff72e8eb7d3df570
Cemal Sezer <infocemalsezer@gmail.com>
2023-10-27 03:39:06 +0300

    inheritance is done

--- CSharpCourse/Inheritance/Program.cs
+++ CSharpCourse/Inheritance/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿Person[] persons = new Person[3]
+{
+    new Customer
+    {
+        FirstName = "Cemal"
+    }, 
+    new Customer
+    {
+        FirstName= "Sezer"
+    }
+    , new Customer
+    {
+        FirstName="MKA"
+    }
+};
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+
+}
+
+class Studentd : Person
+{
+    public string Departent { get; set; }
+
+}
