922c873d88cdb6219c9c0d8ddae04f0b65a23d90
Cemal Sezer <infocemalsezer@gmail.com>
2023-11-12 20:27:01 +0300

    Lesson example is uploaded

---
+++ CSharp_Exercises/TobetoCourseAcademy/Business/Concretes/CategoryManager.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Concretes;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class CategoryManager
+    {
+        public void Add(Category category)
+        {
+            AdoNetCategoryDal categoryDal = new AdoNetCategoryDal();
+            categoryDal.Add(category);
+        }
+    }
+}
---
+++ CSharp_Exercises/TobetoCourseAcademy/ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concretes;
+using Entities.Concretes;
+using System.ComponentModel;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Category category1 = new Category();
+            category1.Id = 1;
+            category1.Name = "Test";
+
+            CategoryManager categoryManager = new CategoryManager();
+            categoryManager.Add(category1);
+        }
+    }
+}
