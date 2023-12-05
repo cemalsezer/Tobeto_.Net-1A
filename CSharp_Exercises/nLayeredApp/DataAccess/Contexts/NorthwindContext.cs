9f15f072edccd4fffdf52df8704feddd67d16a97
Cemal Sezer <infocemalsezer@gmail.com>
2023-12-05 18:52:34 +0300

    nLayered project is added

---
+++ CSharp_Exercises/nLayeredApp/DataAccess/Contexts/NorthwindContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Entities.Concretes;
+
+namespace DataAccess.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public NorthwindContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ CSharp_Exercises/nLayeredApp/WebAPI/Program.cs
@@ -0,0 +1,36 @@
+using DataAccess;
+using Business;
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddBusinessServices();
+            builder.Services.AddDataAccessServices(builder.Configuration);
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
