ae392ba6045c57b45397b6c88f052a2dff1aee63
Cemal Sezer <infocemalsezer@gmail.com>
2023-12-07 11:46:22 +0300

    Business layer is updated

---
+++ CSharp_Exercises/nLayeredTobetoCourseAcademy/Business/BusinessServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public static class BusinessServiceRegistration
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductManager>();
+            return services;
+        }
+    }
+}
