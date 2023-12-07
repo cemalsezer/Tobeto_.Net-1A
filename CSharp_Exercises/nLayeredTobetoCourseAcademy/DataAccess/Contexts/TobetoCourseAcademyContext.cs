9645ea815c5c483bfa705ffdaefe545a31035804
Cemal Sezer <infocemalsezer@gmail.com>
2023-12-07 11:45:19 +0300

    DataAccess files are updated

---
+++ CSharp_Exercises/nLayeredTobetoCourseAcademy/DataAccess/Contexts/TobetoCourseAcademyContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Contexts
+{
+    public class TobetoCourseAcademyContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public TobetoCourseAcademyContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ CSharp_Exercises/nLayeredTobetoCourseAcademy/DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class DataAccessServiceRegistration
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<NorthwindContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+            //services.AddDbContext<BaseDbContext>(options => options.UseSq lServer(configuration.GetConnectionString("RentACar")));
+            services.AddScoped<IProductDal, EfProductDal>();
+            return services;
+        }
+    }
+}
