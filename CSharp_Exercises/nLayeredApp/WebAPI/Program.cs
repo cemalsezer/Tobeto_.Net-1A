b7f20f03f38585fc71370762aeb15dab22b111b6
Cemal Sezer <infocemalsezer@gmail.com>
2023-12-12 23:37:01 +0300

    webapi is upadated

--- CSharp_Exercises/nLayeredApp/WebAPI/Program.cs
+++ CSharp_Exercises/nLayeredApp/WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using DataAccess;
 using Business;
-using Business.Mapping;
+using Business.Profiles;
+
 
 namespace WebAPI
 {
@@ -11,7 +12,7 @@ namespace WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddAutoMapper(typeof(ProductMapping));
+            //builder.Services.AddAutoMapper(typeof(ProductMapping));
             builder.Services.AddBusinessServices();
             builder.Services.AddDataAccessServices(builder.Configuration);
             builder.Services.AddControllers();
