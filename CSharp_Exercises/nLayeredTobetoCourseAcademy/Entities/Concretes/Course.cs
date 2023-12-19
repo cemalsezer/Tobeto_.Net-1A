10a593f3713198f49d287e07095e44abdbfbd4fa
cemalsezer <infocemalsezer@gmail.com>
2023-12-19 23:19:33 +0300

    entity layer is updated

--- CSharp_Exercises/nLayeredTobetoCourseAcademy/Entities/Concretes/Course.cs
+++ CSharp_Exercises/nLayeredTobetoCourseAcademy/Entities/Concretes/Course.cs
@@ -14,7 +14,7 @@ namespace Entities.Concretes
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal UnitPrice { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageUrl { get; set; } 
 
         public Category Category { get; set; }
         public Instructor Instructor { get; set; }
