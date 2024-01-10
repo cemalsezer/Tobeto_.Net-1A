9254228b7acd2c0f4bbce434388204359d826359
cemalsezer <infocemalsezer@gmail.com>
2024-01-10 23:27:14 +0300

    devtools extension package is added

--- React_Exercises/camp-project/src/store/myStore.js
+++ React_Exercises/camp-project/src/store/myStore.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { devToolsEnhancer } from "@redux-devtools/extension";
 import rootReducer from "./rootReducer";
 
 export function store() {
-  return configureStore({reducer:rootReducer})
+  return configureStore({reducer:rootReducer, devToolsEnhancer})
 };
