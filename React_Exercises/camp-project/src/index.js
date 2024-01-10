543b603f2637bc04d1c247a6b636b3323b46b783
cemalsezer <infocemalsezer@gmail.com>
2024-01-10 18:33:23 +0300

    store file is updated

--- React_Exercises/camp-project/src/index.js
+++ React_Exercises/camp-project/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "./store/myStore";
 import "react-toastify/dist/ReactToastify.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- React_Exercises/camp-project/src/store/myStore.js
+++ React_Exercises/camp-project/src/store/myStore.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { devToolsEnhancer } from "@redux-devtools/extension";
 import rootReducer from "./rootReducer";
 
 export function store() {
